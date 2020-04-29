4a3fe766f057dfecf66069dd71fdd8ea8eb7c1bf
Rakesh Kumar <rkrakesh133@gmail.com>
2020-04-29 23:34:49 +0530

    Improved header, navigation, static page

--- private/shared/public_header.php
+++ private/shared/public_header.php
@@ -3,17 +3,9 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title><?= $page_title; ?></title>
-    <!-- <link rel="stylesheet" type="text/css" href="<?= url_for("/css/style.css") ?>" /> -->
     <link rel="stylesheet" type="text/css" href="<?= url_for("/css/bootstrap.min.css") ?>" />
-    <style>
-      img {
-        width: 100px;
-      }
-    </style>
   </head>
   <body>
-    <header style="height:35vh; background: url(<?php echo url_for('/images/background.jpg') ?>) no-repeat center center; background-size: cover;">
-      <div class="container">
-          <!-- <img src="<?php echo url_for('/images/header-logo.png'); ?>" alt="header-logo"> -->
-      </div>
+    <header>
+      <img class="img-fluid" src="<?= url_for('/images/background.jpg'); ?>" alt="house-image">
     </header>
--- private/shared/public_navigation.php
+++ private/shared/public_navigation.php
@@ -1,13 +1,11 @@
 
-<div class="container">
-    <div class="row">
-        <div class="col-12">
-            <nav class="nav nav-pills justify-content-end bg-light navbar-light flex-column flex-sm-row p-2">
-                    <a class="nav-item nav-link" href="<?php url_for(); ?>">Home</a>
-                    <a class="nav-item nav-link" href="#">About Us</a>
-                    <a class="nav-item nav-link bg-danger text-white" href="#">Rules & Regulations For Tenent</a>
-                    <a class="nav-item nav-link" href="#">Contact Us</a>
-            </nav>
+<nav class="nav navbar-dark navbar-expand-sm" style="background-color: #8F939E;">
+    <div class="container">
+        <div class="navbar-nav nav-fill p-1">
+            <a class="nav-item nav-link" href="<?php url_for(); ?>">Home</a>
+            <a class="nav-item nav-link" href="#">About Us</a>
+            <a class="nav-item nav-link bg-danger rounded-pill" href="#">Rules & Regulations For Tenent</a>
+            <a class="nav-item nav-link" href="#">Contact Us</a>
         </div>
     </div>
-</div>
+</nav>
